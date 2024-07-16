aba04e191b301f177d826162378d0c2f458c8212
AutomaticOrca <liangjiaying1013@gmail.com>
2024-07-16 17:28:06 +1000

    frontend: optimized auth part, useContext, auth-hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { useCallback, useContext, useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,56 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 import { AuthContext } from "./shared/context/auth-context";
-
+import { useAuth } from "./shared/hooks/auth-hook";
 import CommonLayout from "./shared/layouts/CommonLayout";
 import Home from "./shared/pages/Home";
 import Auth from "./user/pages/Auth";
 import UserProfile from "./user/pages/UserProfile";
 
 const App = () => {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-
-  // const login = useCallback((uid, token, expirationDate) => {
-  const login = useCallback((uid, token) => {
-    setToken(token);
-    setUserId(uid);
-    // const tokenExpirationDate =
-    //   expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    // setTokenExpirationDate(tokenExpirationDate);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        // expiration: tokenExpirationDate.toISOString()
-      })
-    );
-  }, []);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    // setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  }, []);
+  const { token, login, logout, userId } = useAuth();
 
   const ProtectedRoute = ({ element: Component, ...rest }) => {
     return token ? <Component {...rest} /> : <Navigate to="/auth" />;
   };
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token
-      // storedData.token &&
-      // new Date(storedData.expiration) > new Date()
-    ) {
-      // login(storedData.userId, storedData.token, new Date(storedData.expiration));
-      login(storedData.userId, storedData.token);
-    }
-  }, [login]);
 
   return (
     <AuthContext.Provider
