8b7a756a2166e36751a9ada59ccba630249f0251
AutomaticOrca <liangjiaying1013@gmail.com>
2024-07-21 00:28:26 +1000

    sessions: comingsoon

--- backend/controllers/sessions-controllers.js
+++ backend/controllers/sessions-controllers.js
@@ -15,7 +15,6 @@ const getNext30DaysSession = async (req, res) => {
         ],
       },
     });
-    console.log(sessions);
     res.status(200).json({
       sessions: sessions.map((session) => session.toObject({ getters: true })),
     });
@@ -24,7 +23,27 @@ const getNext30DaysSession = async (req, res) => {
   }
 };
 
-const getNext90DaysSession = () => {};
+const getNext90DaysSession = async (req, res) => {
+  try {
+    const today = new Date();
+    const next90Days = new Date();
+    next90Days.setDate(today.getDate() + 90);
+
+    const sessions = await Session.find({
+      $expr: {
+        $and: [
+          { $gte: [{ $toDate: "$date" }, today] },
+          { $lt: [{ $toDate: "$date" }, next90Days] },
+        ],
+      },
+    });
+    res.status(200).json({
+      sessions: sessions.map((session) => session.toObject({ getters: true })),
+    });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 
 exports.getNext30DaysSession = getNext30DaysSession;
 exports.getNext90DaysSession = getNext90DaysSession;
