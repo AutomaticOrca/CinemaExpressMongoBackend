fdbfd5af5c11847a5386251728c6a4e93589a059
AutomaticOrca <liangjiaying1013@gmail.com>
2024-09-01 01:03:52 +1000

    backend update purchase models

--- backend/models/purchase.js
+++ backend/models/purchase.js
@@ -1,15 +1,50 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const purchaseSchema = new mongoose.Schema({
+const TicketSchema = new Schema({
+  type: {
+    type: String,
+    required: true,
+    enum: ["NORMAL", "DISCOUNTED"],
+  },
+  number: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const purchaseSchema = new Schema({
   sessionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Session",
     required: true,
   },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  number: { type: Number, required: true },
-  isChecked: { type: Boolean, default: false },
-  purchaseDate: { type: Date, default: Date.now },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  tickets: {
+    type: [TicketSchema],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["PENDING", "PAID", "CANCELLED", "REFUNDED"],
+    default: "PENDING",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Purchase = mongoose.model("Purchase", purchaseSchema);
