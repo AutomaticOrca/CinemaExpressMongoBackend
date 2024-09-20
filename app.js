8dc03a02573b97c37634ee1cb1d6af5a51df8a66
AutomaticOrca <liangjiaying1013@gmail.com>
2024-09-20 16:47:23 +1000

    listen to 0.0.0.0

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ mongoose
   )
   .then(() => {
     console.log("connect to mongo");
-    app.listen(5005);
+    app.listen(5005, "0.0.0.0");
+    console.log("listen to 5005");
   })
   .catch((error) => {
     console.log(error);
